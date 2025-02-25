4e6916a9b6c39be4f15bacde8381a8bce2531b1d
Jyothis-Tency <jyothisgtency@gmail.com>
2025-02-25 15:26:40 +0530

    Ready for host - 1

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
+  server: {
+    port: 5173,
+  },
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
